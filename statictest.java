1c653a6c619eff8d88a1a5743f145c33654a877e
Manoj Shiv <manojshivaprakash10@gmail.com>
2024-07-08 00:21:56 +0530

    Use of static keyword

---
+++ statictest.java
@@ -0,0 +1,15 @@
+public class statictest {
+
+    static void test(){
+        System.out.println("This is a static method");
+    }
+
+    public static void main(String[] args) {
+
+        // statictest stat = new statictest();
+        // stat.test();
+
+        test();
+        
+    }
+}
